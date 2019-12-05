825dcf78bd8333f5d366283d5179022c7813d6bb
ailab <chenyilin0110@gmail.com>
2019-12-05 15:22:24 +0800

    add calculate avg file

--- src/SNDAE_NSL-KDD/calavg.py
+++ src/SNDAE_NSL-KDD/calavg.py
@@ -4,7 +4,6 @@ import sys
 run = sys.argv[1]
 name = sys.argv[2]
 data = np.loadtxt('src/SNDAE_NSL-KDD/result/' + name + '.txt', delimiter='\n')
-print("max index, max value =", np.argmax(data), ",", np.max(data))
 avg_list = []
 
 count = 0
@@ -17,4 +16,13 @@ for i in range(len(data)):
         if i == len(data) - 1:
             avg_list.append(count/int(run))
 
+# tree
+# for each_tree in range(len(avg_list)):
+#     print("tree =",(each_tree + 1) * 10,avg_list[each_tree])
+
+# lr
+for each_lr in range(len(avg_list)):
+    print("lr =",(each_lr + 1) / 100,avg_list[each_lr])
+
+print("max index, max value =", np.argmax(data), ",", np.max(data))
 print("avg: tree =", (avg_list.index(max(avg_list)) + 1 ) * int(run), ",", max(avg_list))

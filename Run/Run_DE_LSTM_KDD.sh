c8498d4bf23426d67703788489475a0bf35e4e8b
ailab <chenyilin0110@gmail.com>
2019-12-27 11:18:41 +0800

    revise the run in DE_LSTM_KDD shell script

--- Run/Run_DE_LSTM_KDD.sh
+++ Run/Run_DE_LSTM_KDD.sh
@@ -10,8 +10,8 @@ population="20"
 F="0.5"
 CR="0.3"
 
-END=10
+END=2
 for i in $(seq 1 $END);
 do
-    python3 src/DE_LSTM_KDD99/main.py $train $test $hiddenLayer $outputLayer $batchSize $iteration $epoch $population $F $CR
+    python3 src/DE_LSTM_KDD99_cuda/main.py $train $test $hiddenLayer $outputLayer $batchSize $iteration $epoch $population $F $CR
 done

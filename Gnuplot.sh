5719331c344eebbb2af5e23b3b74e5255f9acfe4
chenyilin0110 <a0989735018@gmail.com>
2020-02-04 10:02:40 +0800

    add Gnuplot shell script file

---
+++ Gnuplot.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# KDD99 2 categories
+gnuplot src/Plot/KDD99/2class/Bar_Graph.plt
+
+# KDD99 5 categories
+gnuplot src/Plot/KDD99/5class/Bar_Graph.plt
+
+# NSL-KDD-1parameter 2 categories
+gnuplot src/Plot/NSL-KDD-1parameter/2class/Bar_Graph.plt
+gnuplot src/Plot/NSL-KDD-1parameter/2class/Bar_Graph-21.plt
+
+# NSL-KDD-1parameter 5 categories
+gnuplot src/Plot/NSL-KDD-1parameter/5class/Bar_Graph.plt
+gnuplot src/Plot/NSL-KDD-1parameter/5class/Bar_Graph-21.plt
+
+# NSL-KDD-2parameters 2 categories
+gnuplot src/Plot/NSL-KDD-2parameters/2class/Bar_Graph.plt
+gnuplot src/Plot/NSL-KDD-2parameters/2class/Bar_Graph-21.plt
+
+# NSL-KDD-2parameters 5 categories
+gnuplot src/Plot/NSL-KDD-2parameters/5class/Bar_Graph.plt
+gnuplot src/Plot/NSL-KDD-2parameters/5class/Bar_Graph-21.plt

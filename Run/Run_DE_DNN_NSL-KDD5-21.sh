3d38df8f7ebd2293c4673b8c5de9c5f80a473af4
ailab <chenyilin0110@gmail.com>
2020-02-09 22:23:10 +0800

    add DE_DNN_NSL-KDD shell script files

---
+++ Run/Run_DE_DNN_NSL-KDD5-21.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+train="train"
+test="test-21"
+hiddenLayer="1"
+outputLayer="5"
+iteration="40"
+epoch="100"
+population="20"
+F="0.5"
+CR="0.3"
+name="-21"
+
+END=5
+for i in $(seq 1 $END);
+do
+    python3.6 src/DE_DNN_NSL-KDD/main.py $train $test $hiddenLayer $outputLayer $iteration $epoch $population $F $CR $name
+done

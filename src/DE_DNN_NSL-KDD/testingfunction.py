4007d50c6d02f607c6dfb5477d6b132ee05184fa
ailab <chenyilin0110@gmail.com>
2020-02-10 13:42:38 +0800

    add testingfunction file and add the new argv in the main file for saving test and test-21 model

---
+++ src/DE_DNN_NSL-KDD/testingfunction.py
@@ -0,0 +1,16 @@
+import numpy as np
+import torch.nn as nn
+import torch
+from accuracyfunction import accuracy
+
+def testing(hiddenLayer, outputLayer, x_test_tensor, y_test_tensor, name):
+    if name == '0':
+        net = torch.load('src/DE_DNN_NSL-KDD/result/DE_DNN_' + outputLayer + '.pkl')
+    else:
+        net = torch.load('src/DE_DNN_NSL-KDD/result/DE_DNN_' + outputLayer + name + '.pkl')
+    y_test_predic = net(x_test_tensor, int(hiddenLayer))
+    pred = y_test_predic.detach().numpy()
+    y_test_list_predic = np.argmax(pred, axis=1)
+
+    accuracyvalue = accuracy(y_test_tensor, y_test_list_predic)
+    print(accuracyvalue)

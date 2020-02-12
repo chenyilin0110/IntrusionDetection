2eda19cb50b8e33abd1e9b171ba35c3987d21724
ailab <chenyilin0110@gmail.com>
2020-02-12 13:54:40 +0800

    add Run shell script for HC_LSTM_NSL-KDD 2&5 categories and HC_LSTM_NSL-KDD-21

---
+++ Run/Run_HC_LSTM_NSL-KDD2-21.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+train="train"
+test="test-21"
+hiddenLayer="1"
+outputLayer="2"
+iteration="800"
+batchSize="10000"
+epoch="100"
+name="-21"
+
+END=3
+for i in $(seq 1 $END);
+do
+    python3.6 src/HC_LSTM_NSL-KDD/main.py $train $test $hiddenLayer $outputLayer $iteration $batchSize $epoch $name
+done

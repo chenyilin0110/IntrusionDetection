2c82c39cf27045f5408952cd883cd75bd4be79c1
chenyilin0110 <a0989735018@gmail.com>
2020-02-16 11:26:48 +0800

    change the calavg file input

--- src/Calculate/calavg.py
+++ src/Calculate/calavg.py
@@ -2,12 +2,14 @@ import numpy as np
 import sys
 
 run = sys.argv[1]
-folder = sys.argv[2]
-file_name = sys.argv[3]
-data = np.loadtxt('src/' + folder + '/result/' + file_name + '.txt', delimiter='\n')
+algorithm = sys.argv[2]
+dataset = sys.argv[3]
+file_name = sys.argv[4]
+data = np.loadtxt('src/' + algorithm + '_' + dataset + '/result/' + file_name + '.txt', delimiter='\n')
+
 avg_list = []
 
 count = 0
 for i in range(len(data)):
     count += data[i]
-print(count/int(run))
+print(count/int(run), end=' ')

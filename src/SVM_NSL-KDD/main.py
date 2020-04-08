0798ae7a7dbaf130d250eeedc13f553e0af82e6c
chenyilin0110 <a0989735018@gmail.com>
2020-04-08 20:28:35 +0800

    add SVM algorithm

---
+++ src/SVM_NSL-KDD/main.py
@@ -0,0 +1,87 @@
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+from torch.autograd import Variable
+import torch.optim as optim
+import torch.nn as nn
+import torch
+import numpy as np
+import sys
+import preprocess
+import accuracyfunction
+#import matplotlib.pyplot as plt
+#import seaborn as sns
+
+# don't show warnings message
+import warnings
+warnings.filterwarnings("ignore")
+
+# set filename outputLayer testing epoch
+trainData = sys.argv[1]
+testData = sys.argv[2]
+outputLayer = sys.argv[3]
+c = sys.argv[4]
+
+# load trainData
+train_temp = preprocess.loadDataset(trainData)
+train_noStringTemp_X = train_temp[0:,0:-1]
+
+# start preprocess----------------------------------------------------------------------------------------------------------
+
+# only trainData do this
+protocal_type_list = preprocess.saveProtocal_typeOrder(train_noStringTemp_X)
+service_list = preprocess.saveServiceOrder(train_noStringTemp_X)
+flag_list = preprocess.saveFlagOrder(train_noStringTemp_X)
+protocal_type_onehotencoded = preprocess.onehotencoded(protocal_type_list)
+service_list_onehotencoded = preprocess.onehotencoded(service_list)
+flag_list_onehotencoded = preprocess.onehotencoded(flag_list)
+
+# load testData
+test_temp = preprocess.loadDataset(testData)
+test_noStringTemp_X = test_temp[0:,0:-1]
+
+train_noStringTemp_Y = []
+train_noStringTemp_Y = preprocess.distinguishNaturalAttack(train_temp, train_noStringTemp_Y, int(outputLayer))
+test_noStringTemp_Y = []
+test_noStringTemp_Y = preprocess.distinguishNaturalAttack(test_temp, test_noStringTemp_Y, int(outputLayer))
+
+# np can't dynamic add number so use list
+train_noStringTemp_X = train_noStringTemp_X.tolist() 
+test_noStringTemp_X = test_noStringTemp_X.tolist()
+
+# replace text with onehotencoded number
+preprocess.replace(train_noStringTemp_X, protocal_type_list, service_list, flag_list, protocal_type_onehotencoded, service_list_onehotencoded, flag_list_onehotencoded)
+preprocess.replace(test_noStringTemp_X, protocal_type_list, service_list, flag_list, protocal_type_onehotencoded, service_list_onehotencoded, flag_list_onehotencoded)
+
+# list->np
+train_noStringTemp_X = np.array(train_noStringTemp_X)
+train_noStringTemp_Y = np.array(train_noStringTemp_Y)
+test_noStringTemp_X = np.array(test_noStringTemp_X)
+test_noStringTemp_Y = np.array(test_noStringTemp_Y)
+
+# duration src_bytes
+for i in range(len(train_noStringTemp_X)):
+    train_noStringTemp_X[i][0] = preprocess.log(train_noStringTemp_X[i][0])
+    train_noStringTemp_X[i][4] = preprocess.log(train_noStringTemp_X[i][4])
+    train_noStringTemp_X[i][5] = preprocess.log(train_noStringTemp_X[i][5])
+
+for i in range(len(test_noStringTemp_X)):
+    test_noStringTemp_X[i][0] = preprocess.log(test_noStringTemp_X[i][0])
+    test_noStringTemp_X[i][4] = preprocess.log(test_noStringTemp_X[i][4])
+    test_noStringTemp_X[i][5] = preprocess.log(test_noStringTemp_X[i][5])
+    
+# normalize
+train_resultNormalize = preprocess.normalize(train_noStringTemp_X)
+test_resultNormalize = preprocess.normalize(test_noStringTemp_X)
+
+# end preprocess----------------------------------------------------------------------------------------------------------
+
+# build svm
+svm = svm.SVC(C=float(c))
+
+# training
+svmFit = svm.fit(train_resultNormalize, train_noStringTemp_Y)
+
+# testing 
+y_test_prediction = svm.predict(test_resultNormalize)
+
+accuracyfunction.accuracy(test_noStringTemp_Y, y_test_prediction)

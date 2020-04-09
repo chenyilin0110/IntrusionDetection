f9d31169d7c46d18ad30aea5f138f458afd0bded
chenyilin0110 <a0989735018@gmail.com>
2020-04-09 19:22:20 +0800

    add Calculate & Gnuplot shell script files

--- script/Calculate.sh
+++ script/Calculate.sh
@@ -1,68 +1,86 @@
 #!/bin/bash
+rm src/Plot/NSL-KDD/2class/NSL-*
+rm src/Plot/NSL-KDD/5class/NSL-*
+# rm src/Plot/KDD99/2class/KDD*
+# rm src/Plot/KDD99/5class/KDD*
 
 # NSL-KDD 2 categories
 dataset="NSL-KDD"
 file_name="experimental_result_2"
 path="src/Plot/NSL-KDD/2class/NSL-KDD_2class_Avg.txt"
-echo '"'"NSL-KDD 2 categories"'"' >> $path
+echo -n '"'"NSL-KDD 2 categories"'" ' >> $path
 python3 src/Calculate/calavg.py 5 DAE $dataset $file_name >> $path
 python3 src/Calculate/calavg.py 5 SNDAE $dataset $file_name >> $path
 python3 src/Calculate/calavg.py 15 DNN $dataset $file_name >> $path
-python3 src/Calculate/calavg.py 10 HC_DNN $dataset $file_name >> $path
+# python3 src/Calculate/calavg.py 10 HC_DNN $dataset $file_name >> $path
 python3 src/Calculate/calavg.py 5 DE_DNN $dataset $file_name >> $path
 python3 src/Calculate/calavg.py 15 LSTM $dataset $file_name >> $path
-python3 src/Calculate/calavg.py 3 HC_LSTM $dataset $file_name >> $path
-python3 src/Calculate/calavg.py 3 DE_LSTM_one_parameter $dataset $file_name >> $path
-python3 src/Calculate/calavg.py 3 DE_LSTM_two_parameters $dataset $file_name >> $path
+# python3 src/Calculate/calavg.py 3 HC_LSTM $dataset $file_name >> $path
+python3 src/Calculate/calavg.py 4 DE_LSTM_one_parameter $dataset $file_name >> $path
+python3 src/Calculate/calavg.py 4 DE_LSTM_two_parameters $dataset $file_name >> $path
 python3 src/Calculate/calavg.py 4 EDE_LSTM $dataset $file_name >> $path
+python3 src/Calculate/calavg.py 5 Naive_Bayes $dataset $file_name >> $path
+python3 src/Calculate/calavg.py 5 RandomForest $dataset $file_name >> $path
+python3 src/Calculate/calavg.py 5 SVM $dataset $file_name >> $path
 
 # NSL-KDD 5 categories
 dataset="NSL-KDD"
 file_name="experimental_result_5"
 path="src/Plot/NSL-KDD/5class/NSL-KDD_5class_Avg.txt"
-echo '"'"NSL-KDD 5 categories"'"' >> $path
+echo -n '"'"NSL-KDD 5 categories"'" ' >> $path
 python3 src/Calculate/calavg.py 5 DAE $dataset $file_name >> $path
 python3 src/Calculate/calavg.py 5 SNDAE $dataset $file_name >> $path
 python3 src/Calculate/calavg.py 15 DNN $dataset $file_name >> $path
-python3 src/Calculate/calavg.py 10 HC_DNN $dataset $file_name >> $path
+# python3 src/Calculate/calavg.py 10 HC_DNN $dataset $file_name >> $path
 python3 src/Calculate/calavg.py 5 DE_DNN $dataset $file_name >> $path
 python3 src/Calculate/calavg.py 15 LSTM $dataset $file_name >> $path
-python3 src/Calculate/calavg.py 3 HC_LSTM $dataset $file_name >> $path
+# python3 src/Calculate/calavg.py 3 HC_LSTM $dataset $file_name >> $path
 python3 src/Calculate/calavg.py 3 DE_LSTM_one_parameter $dataset $file_name >> $path
-python3 src/Calculate/calavg.py 3 DE_LSTM_two_parameters $dataset $file_name >> $path
+python3 src/Calculate/calavg.py 4 DE_LSTM_two_parameters $dataset $file_name >> $path
 python3 src/Calculate/calavg.py 4 EDE_LSTM $dataset $file_name >> $path
+python3 src/Calculate/calavg.py 5 Naive_Bayes $dataset $file_name >> $path
+python3 src/Calculate/calavg.py 5 RandomForest $dataset $file_name >> $path
+python3 src/Calculate/calavg.py 5 SVM $dataset $file_name >> $path
 
 # NSL-KDD 2-21 categories
 dataset="NSL-KDD"
 file_name="experimental_result_2-21"
 path="src/Plot/NSL-KDD/2class/NSL-KDD_2class_Avg-21.txt"
-echo '"'"NSL-KDD 2 categories-21"'"' >> $path
+echo -n '"'"NSL-KDD 2 categories-21"'" ' >> $path
 python3 src/Calculate/calavg.py 5 DAE $dataset $file_name >> $path
 python3 src/Calculate/calavg.py 5 SNDAE $dataset $file_name >> $path
 python3 src/Calculate/calavg.py 15 DNN $dataset $file_name >> $path
-python3 src/Calculate/calavg.py 10 HC_DNN $dataset $file_name >> $path
+# python3 src/Calculate/calavg.py 10 HC_DNN $dataset $file_name >> $path
 python3 src/Calculate/calavg.py 5 DE_DNN $dataset $file_name >> $path
 python3 src/Calculate/calavg.py 15 LSTM $dataset $file_name >> $path
-python3 src/Calculate/calavg.py 3 HC_LSTM $dataset $file_name >> $path
-python3 src/Calculate/calavg.py 3 DE_LSTM_one_parameter $dataset $file_name >> $path
+# python3 src/Calculate/calavg.py 3 HC_LSTM $dataset $file_name >> $path
+echo -n "0 " >> $path
+# python3 src/Calculate/calavg.py 3 DE_LSTM_one_parameter $dataset $file_name >> $path
 python3 src/Calculate/calavg.py 3 DE_LSTM_two_parameters $dataset $file_name >> $path
 python3 src/Calculate/calavg.py 4 EDE_LSTM $dataset $file_name >> $path
+python3 src/Calculate/calavg.py 5 Naive_Bayes $dataset $file_name >> $path
+python3 src/Calculate/calavg.py 5 RandomForest $dataset $file_name >> $path
+python3 src/Calculate/calavg.py 5 SVM $dataset $file_name >> $path
 
 # NSL-KDD 5-21 categories
 dataset="NSL-KDD"
 file_name="experimental_result_5-21"
 path="src/Plot/NSL-KDD/5class/NSL-KDD_5class_Avg-21.txt"
-echo '"'"NSL-KDD 5 categories-21"'"' >> $path
+echo -n '"'"NSL-KDD 5 categories-21"'" ' >> $path
 python3 src/Calculate/calavg.py 5 DAE $dataset $file_name >> $path
 python3 src/Calculate/calavg.py 5 SNDAE $dataset $file_name >> $path
 python3 src/Calculate/calavg.py 15 DNN $dataset $file_name >> $path
-python3 src/Calculate/calavg.py 10 HC_DNN $dataset $file_name >> $path
-python3 src/Calculate/calavg.py 5 DE_DNN $dataset $file_name >> $path
+# python3 src/Calculate/calavg.py 10 HC_DNN $dataset $file_name >> $path
+python3 src/Calculate/calavg.py 3 DE_DNN $dataset $file_name >> $path
 python3 src/Calculate/calavg.py 15 LSTM $dataset $file_name >> $path
-python3 src/Calculate/calavg.py 3 HC_LSTM $dataset $file_name >> $path
-python3 src/Calculate/calavg.py 3 DE_LSTM_one_parameter $dataset $file_name >> $path
-python3 src/Calculate/calavg.py 3 DE_LSTM_two_parameters $dataset $file_name >> $path
+# python3 src/Calculate/calavg.py 3 HC_LSTM $dataset $file_name >> $path
+echo -n "0 " >> $path
+# python3 src/Calculate/calavg.py 3 DE_LSTM_one_parameter $dataset $file_name >> $path
+python3 src/Calculate/calavg.py 4 DE_LSTM_two_parameters $dataset $file_name >> $path
 python3 src/Calculate/calavg.py 4 EDE_LSTM $dataset $file_name >> $path
+python3 src/Calculate/calavg.py 5 Naive_Bayes $dataset $file_name >> $path
+python3 src/Calculate/calavg.py 5 RandomForest $dataset $file_name >> $path
+python3 src/Calculate/calavg.py 5 SVM $dataset $file_name >> $path
 
 # # KDD99 2 categories
 # dataset="KDD99"
--- script/Gnuplot.sh
+++ script/Gnuplot.sh
@@ -6,18 +6,18 @@ gnuplot src/Plot/KDD99/2class/Bar_Graph.plt
 # KDD99 5 categories
 gnuplot src/Plot/KDD99/5class/Bar_Graph.plt
 
-# NSL-KDD-1parameter 2 categories
-gnuplot src/Plot/NSL-KDD-1parameter/2class/Bar_Graph.plt
-gnuplot src/Plot/NSL-KDD-1parameter/2class/Bar_Graph-21.plt
+# NSL-KDD 2 categories
+gnuplot src/Plot/NSL-KDD/2class/Bar_Graph.plt
+gnuplot src/Plot/NSL-KDD/2class/Bar_Graph-21.plt
 
-# NSL-KDD-1parameter 5 categories
-gnuplot src/Plot/NSL-KDD-1parameter/5class/Bar_Graph.plt
-gnuplot src/Plot/NSL-KDD-1parameter/5class/Bar_Graph-21.plt
+# NSL-KDD 5 categories
+gnuplot src/Plot/NSL-KDD/5class/Bar_Graph.plt
+gnuplot src/Plot/NSL-KDD/5class/Bar_Graph-21.plt
 
-# NSL-KDD-2parameters 2 categories
-gnuplot src/Plot/NSL-KDD-2parameters/2class/Bar_Graph.plt
-gnuplot src/Plot/NSL-KDD-2parameters/2class/Bar_Graph-21.plt
+# # NSL-KDD-2parameters 2 categories
+# gnuplot src/Plot/NSL-KDD-2parameters/2class/Bar_Graph.plt
+# gnuplot src/Plot/NSL-KDD-2parameters/2class/Bar_Graph-21.plt
 
-# NSL-KDD-2parameters 5 categories
-gnuplot src/Plot/NSL-KDD-2parameters/5class/Bar_Graph.plt
-gnuplot src/Plot/NSL-KDD-2parameters/5class/Bar_Graph-21.plt
+# # NSL-KDD-2parameters 5 categories
+# gnuplot src/Plot/NSL-KDD-2parameters/5class/Bar_Graph.plt
+# gnuplot src/Plot/NSL-KDD-2parameters/5class/Bar_Graph-21.plt

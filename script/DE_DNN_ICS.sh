77f31151840bc800d66a26bda3ad9455b609b09f
chenyilin0110 <a0989735018@gmail.com>
2020-04-19 12:05:33 +0800

    add the CUDA_VISIBLE_DEVICES in the file

--- script/DE_DNN_ICS.sh
+++ script/DE_DNN_ICS.sh
@@ -10,57 +10,73 @@ CR=0.3
 
 data=15
 END=10
+mid=`expr $END / 2`
 
-# DAE 2 categories
+# DE_DNN 2 categories
 finder="2"
 outputLayer="2"
 for j in $(seq 1 $data);
 do
     filename="data"$j".csv"
-    path="src/DE_DNN_ICS/result/experimental_result_"$outputLayer"-"$j".txt"
     for i in $(seq 1 $END);
     do
-        if [ "$i" = "$END" ]
+        path="src/DE_DNN_ICS/result/experimental_result_"$outputLayer"-"$j"-"$i".txt"
+        if [ "$i" -gt "$mid" ]
         then
-            python3 src/DE_DNN_ICS/main.py $finder $filename $outputLayer $hiddenLayer $hiddenNeural $test $iteration $epoch $population $F $CR $j >> $path
+            if [ "$i" = "$END" ]
+            then
+                CUDA_VISIBLE_DEVICES=1 python3 src/DE_DNN_ICS/main.py $finder $filename $outputLayer $hiddenLayer $hiddenNeural $test $iteration $epoch $population $F $CR $j >> $path
+            else
+                CUDA_VISIBLE_DEVICES=1 python3 src/DE_DNN_ICS/main.py $finder $filename $outputLayer $hiddenLayer $hiddenNeural $test $iteration $epoch $population $F $CR $j >> $path &
+            fi            
         else
-            python3 src/DE_DNN_ICS/main.py $finder $filename $outputLayer $hiddenLayer $hiddenNeural $test $iteration $epoch $population $F $CR $j >> $path &
+            CUDA_VISIBLE_DEVICES=0 python3 src/DE_DNN_ICS/main.py $finder $filename $outputLayer $hiddenLayer $hiddenNeural $test $iteration $epoch $population $F $CR $j >> $path &
         fi
     done
 done
 
-# DAE 5 categories
-finder="5"
-outputLayer="5"
+# DE_DNN 3 categories
+finder="3"
+outputLayer="3"
 for j in $(seq 1 $data);
 do
     filename="data"$j".csv"
-    path="src/DE_DNN_ICS/result/experimental_result_"$outputLayer"-"$j".txt"
     for i in $(seq 1 $END);
     do
-        if [ "$i" = "$END" ]
+        path="src/DE_DNN_ICS/result/experimental_result_"$outputLayer"-"$j"-"$i".txt"
+        if [ "$i" -gt "$mid" ]
         then
-            python3 src/DE_DNN_ICS/main.py $finder $filename $outputLayer $hiddenLayer $hiddenNeural $test $iteration $epoch $population $F $CR $j >> $path
+            if [ "$i" = "$END" ]
+            then
+                CUDA_VISIBLE_DEVICES=1 python3 src/DE_DNN_ICS/main.py $finder $filename $outputLayer $hiddenLayer $hiddenNeural $test $iteration $epoch $population $F $CR $j >> $path
+            else
+                CUDA_VISIBLE_DEVICES=1 python3 src/DE_DNN_ICS/main.py $finder $filename $outputLayer $hiddenLayer $hiddenNeural $test $iteration $epoch $population $F $CR $j >> $path &
+            fi            
         else
-            python3 src/DE_DNN_ICS/main.py $finder $filename $outputLayer $hiddenLayer $hiddenNeural $test $iteration $epoch $population $F $CR $j >> $path &
+            CUDA_VISIBLE_DEVICES=0 python3 src/DE_DNN_ICS/main.py $finder $filename $outputLayer $hiddenLayer $hiddenNeural $test $iteration $epoch $population $F $CR $j >> $path &
         fi
     done
 done
 
-# DAE multi categories
+# DE_DNN multi categories
 finder="multi"
-outputLayer="multi"
+outputLayer="41"
 for j in $(seq 1 $data);
 do
     filename="data"$j".csv"
-    path="src/DE_DNN_ICS/result/experimental_result_"$outputLayer"-"$j".txt"
     for i in $(seq 1 $END);
     do
-        if [ "$i" = "$END" ]
+        path="src/DE_DNN_ICS/result/experimental_result_"$outputLayer"-"$j"-"$i".txt"
+        if [ "$i" -gt "$mid" ]
         then
-            python3 src/DE_DNN_ICS/main.py $finder $filename $outputLayer $hiddenLayer $hiddenNeural $test $iteration $epoch $population $F $CR $j >> $path
+            if [ "$i" = "$END" ]
+            then
+                CUDA_VISIBLE_DEVICES=1 python3 src/DE_DNN_ICS/main.py $finder $filename $outputLayer $hiddenLayer $hiddenNeural $test $iteration $epoch $population $F $CR $j >> $path
+            else
+                CUDA_VISIBLE_DEVICES=1 python3 src/DE_DNN_ICS/main.py $finder $filename $outputLayer $hiddenLayer $hiddenNeural $test $iteration $epoch $population $F $CR $j >> $path &
+            fi            
         else
-            python3 src/DE_DNN_ICS/main.py $finder $filename $outputLayer $hiddenLayer $hiddenNeural $test $iteration $epoch $population $F $CR $j >> $path &
+            CUDA_VISIBLE_DEVICES=0 python3 src/DE_DNN_ICS/main.py $finder $filename $outputLayer $hiddenLayer $hiddenNeural $test $iteration $epoch $population $F $CR $j >> $path &
         fi
     done
 done

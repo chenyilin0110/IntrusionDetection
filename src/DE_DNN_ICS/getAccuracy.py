690a459df273da4ab9e2284e6300d52385f4e886
chenyilin0110 <a0989735018@gmail.com>
2020-04-20 17:40:21 +0800

    add getACC file

--- src/DE_DNN_ICS/getAccuracy.py
+++ src/DE_DNN_ICS/getAccuracy.py
@@ -8,18 +8,27 @@ run = sys.argv[4]
 
 # copy acc to other file 2 categories
 for i in range(int(datasetNumber)):
+    f = open('src/' + algorithm +'_' + dataset + '/result/' + 'experimental_result_2-' + str(i+1) + '.txt', "a")
     for j in range(int(run)):
         data = np.loadtxt('src/' + algorithm + '_' + dataset + '/result/' + 'experimental_result_2-' + str((i+1)) + '-' + str(j+1) + '.txt', delimiter='\n')
-        print(data[-1])
+        f.write(str(data[-1]))
+        f.write('\n')
+    f.close()
 
 # copy acc to other file 3 categories
 for i in range(int(datasetNumber)):
+    f = open('src/' + algorithm +'_' + dataset + '/result/' + 'experimental_result_3-' + str(i+1) + '.txt', "a")
     for j in range(int(run)):
         data = np.loadtxt('src/' + algorithm + '_' + dataset + '/result/' + 'experimental_result_3-' + str((i+1)) + '-' + str(j+1) + '.txt', delimiter='\n')
-        print(data[-1])
+        f.write(str(data[-1]))
+        f.write('\n')
+    f.close()
 
 # copy acc to other file multi categories
 for i in range(int(datasetNumber)):
+    f = open('src/' + algorithm +'_' + dataset + '/result/' + 'experimental_result_41-' + str(i+1) + '.txt', "a")
     for j in range(int(run)):
         data = np.loadtxt('src/' + algorithm + '_' + dataset + '/result/' + 'experimental_result_41-' + str((i+1)) + '-' + str(j+1) + '.txt', delimiter='\n')
-        print(data[-1])
+        f.write(str(data[-1]))
+        f.write('\n')
+    f.close()

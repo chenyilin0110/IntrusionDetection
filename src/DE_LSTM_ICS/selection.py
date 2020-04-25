981de5a753787b680a3a4cd6d0af85f9f9752be7
chenyilin0110 <a0989735018@gmail.com>
2020-04-25 18:38:24 +0800

    add DE_LSTM algorithm in ICS

---
+++ src/DE_LSTM_ICS/selection.py
@@ -0,0 +1,18 @@
+from fitness import fitness
+import numpy as np
+
+def selection(crossoverModel, originalModel, populationDataOriginal, crossoverData, countOriginalLossValue, countCrossoverLossValue, hiddenLayer, outputLayer, epoch, batchSize, train_noStringTemp_X, x_train_tensor, y_train_tensor, cuda):
+    selectionPopulationData = populationDataOriginal
+    
+    for eachPopulation in range(np.size(populationDataOriginal,0)):
+        countOriginalLossValue[0][eachPopulation],  model = fitness(populationDataOriginal[eachPopulation], hiddenLayer, outputLayer, epoch, batchSize, train_noStringTemp_X, x_train_tensor, y_train_tensor, cuda)
+        originalModel.append(model)
+
+        countCrossoverLossValue[0][eachPopulation], model = fitness(crossoverData[eachPopulation], hiddenLayer, outputLayer, epoch, batchSize, train_noStringTemp_X, x_train_tensor, y_train_tensor, cuda)
+        crossoverModel.append(model)
+
+    for eachPopulation in range(np.size(countCrossoverLossValue, 1)):
+        if countCrossoverLossValue[0][eachPopulation] <= countOriginalLossValue[0][eachPopulation]:
+            for eachDim in range(hiddenLayer):
+                selectionPopulationData[eachPopulation][eachDim] = crossoverData[eachPopulation][eachDim]
+    return selectionPopulationData

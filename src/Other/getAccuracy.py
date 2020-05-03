490091e872aa659004628404750ddd57e2ebf802
chenyilin0110 <a0989735018@gmail.com>
2020-05-03 10:55:20 +0800

    add categories argv in the getAccuracy file input

--- src/Other/getAccuracy.py
+++ src/Other/getAccuracy.py
@@ -4,31 +4,14 @@ import sys
 algorithm = sys.argv[1]
 dataset = sys.argv[2]
 datasetNumber = sys.argv[3]
-run = sys.argv[4]
+categories = sys.argv[4]
+run = sys.argv[5]
 
-# copy acc to other file 2 categories
+# copy acc to other file 
 for i in range(int(datasetNumber)):
-    f = open('src/' + algorithm +'_' + dataset + '/result/' + 'experimental_result_2-' + str(i+1) + '.txt', "a")
+    f = open('src/' + algorithm +'_' + dataset + '/result/' + 'experimental_result_' + categories + '-' + str(i+1) + '.txt', "a")
     for j in range(int(run)):
-        data = np.loadtxt('src/' + algorithm + '_' + dataset + '/result/' + 'experimental_result_2-' + str((i+1)) + '-' + str(j+1) + '.txt', delimiter='\n')
-        f.write(str(data[-1]))
-        f.write('\n')
-    f.close()
-
-# copy acc to other file 3 categories
-for i in range(int(datasetNumber)):
-    f = open('src/' + algorithm +'_' + dataset + '/result/' + 'experimental_result_3-' + str(i+1) + '.txt', "a")
-    for j in range(int(run)):
-        data = np.loadtxt('src/' + algorithm + '_' + dataset + '/result/' + 'experimental_result_3-' + str((i+1)) + '-' + str(j+1) + '.txt', delimiter='\n')
-        f.write(str(data[-1]))
-        f.write('\n')
-    f.close()
-
-# copy acc to other file multi categories
-for i in range(int(datasetNumber)):
-    f = open('src/' + algorithm +'_' + dataset + '/result/' + 'experimental_result_41-' + str(i+1) + '.txt', "a")
-    for j in range(int(run)):
-        data = np.loadtxt('src/' + algorithm + '_' + dataset + '/result/' + 'experimental_result_41-' + str((i+1)) + '-' + str(j+1) + '.txt', delimiter='\n')
+        data = np.loadtxt('src/' + algorithm + '_' + dataset + '/result/' + 'experimental_result_' + categories + '-' + str((i+1)) + '-' + str(j+1) + '.txt', delimiter='\n')
         f.write(str(data[-1]))
         f.write('\n')
     f.close()

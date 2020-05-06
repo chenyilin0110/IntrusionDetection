5feb2970863711ed6e702f118fbbe221c898bc81
chenyilin0110 <a0989735018@gmail.com>
2020-05-06 14:00:15 +0800

    change the order

--- script/Gnuplot.sh
+++ script/Gnuplot.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-# KDD99 2 categories
-gnuplot src/Plot/KDD99/2class/Bar_Graph.plt
-
-# KDD99 5 categories
-gnuplot src/Plot/KDD99/5class/Bar_Graph.plt
-
 # NSL-KDD 2 categories
 gnuplot src/Plot/NSL-KDD/2class/Bar_Graph.plt
 gnuplot src/Plot/NSL-KDD/2class/Bar_Graph-21.plt
@@ -22,3 +16,9 @@ gnuplot src/Plot/ICS/3class/Bar_Graph.plt
 
 # ICS multi categories
 gnuplot src/Plot/ICS/multi/Bar_Graph.plt
+
+# KDD99 2 categories
+gnuplot src/Plot/KDD99/2class/Bar_Graph.plt
+
+# KDD99 5 categories
+gnuplot src/Plot/KDD99/5class/Bar_Graph.plt

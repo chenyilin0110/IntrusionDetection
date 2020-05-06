9898215c55de0ba8b7c18c64bfcca1110ea2bb05
chenyilin0110 <a0989735018@gmail.com>
2020-05-06 13:52:16 +0800

    add NSL-KDD & KDD99 get accuracy in getAccuracy file

--- src/Other/getAccuracy.py
+++ src/Other/getAccuracy.py
@@ -3,15 +3,24 @@ import sys
 
 algorithm = sys.argv[1]
 dataset = sys.argv[2]
-datasetNumber = sys.argv[3]
-categories = sys.argv[4]
-run = sys.argv[5]
+categories = sys.argv[3]
+run = sys.argv[4]
 
-# copy acc to other file 
-for i in range(int(datasetNumber)):
-    f = open('src/' + algorithm +'_' + dataset + '/result/' + 'experimental_result_' + categories + '-' + str(i+1) + '.txt', "a")
+if dataset == "ICS":
+    datasetNumber = sys.argv[5]
+    # copy acc to other file for ICS dataset
+    for i in range(int(datasetNumber)):
+        f = open('src/' + algorithm +'_' + dataset + '/result/' + 'experimental_result_' + categories + '-' + str(i+1) + '.txt', "a")
+        for j in range(int(run)):
+            data = np.loadtxt('src/' + algorithm + '_' + dataset + '/result/' + 'experimental_result_' + categories + '-' + str((i+1)) + '-' + str(j+1) + '.txt', delimiter='\n')
+            f.write(str(data[-1]))
+            f.write('\n')
+        f.close()
+else:
+    # copy acc to other file for KDD99 & NSL-KDD dataset
+    f = open('src/' + algorithm +'_' + dataset + '/result/' + 'experimental_result_' + categories + '.txt', "a")
     for j in range(int(run)):
-        data = np.loadtxt('src/' + algorithm + '_' + dataset + '/result/' + 'experimental_result_' + categories + '-' + str((i+1)) + '-' + str(j+1) + '.txt', delimiter='\n')
+        data = np.loadtxt('src/' + algorithm + '_' + dataset + '/result/' + 'experimental_result_' + categories + '-' + str(j+1) + '.txt', delimiter='\n')
         f.write(str(data[-1]))
         f.write('\n')
     f.close()

1f7d3d8e9e4d169d704bbd078ca93699190464ec
chenyilin0110 <a0989735018@gmail.com>
2020-05-14 19:03:07 +0800

    add NSL-KDD get each run accuracy value

--- src/Other/getAccuracy.py
+++ src/Other/getAccuracy.py
@@ -16,11 +16,28 @@ if dataset == "ICS":
             f.write(str(data[-1]))
             f.write('\n')
         f.close()
-else:
-    # copy acc to other file for KDD99 & NSL-KDD dataset
+elif dataset == "KDD99":
+    # copy acc to other file for KDD99
     f = open('src/' + algorithm +'_' + dataset + '/result/' + 'experimental_result_' + categories + '.txt', "a")
     for j in range(int(run)):
         data = np.loadtxt('src/' + algorithm + '_' + dataset + '/result/' + 'experimental_result_' + categories + '-' + str(j+1) + '.txt', delimiter='\n')
+        f.write(str(data[-1]))
+        f.write('\n')
+    f.close()
+else:
+    # copy acc to other file for NSL-KDD dataset
+    f = open('src/' + algorithm +'_' + dataset + '/result/' + 'experimental_result_' + categories + '.txt', "a")
+    for j in range(int(run)):
+        data = np.loadtxt('src/' + algorithm + '_' + dataset + '/result/' + 'experimental_result_' + categories + '_bs256_RMSPROP'\
+            '-' + str(j+1) + '.txt', delimiter='\n')
+        f.write(str(data[-1]))
+        f.write('\n')
+    f.close()
+    # Test-21
+    f = open('src/' + algorithm +'_' + dataset + '/result/' + 'experimental_result_' + categories + '-21.txt', "a")
+    for j in range(int(run)):
+        data = np.loadtxt('src/' + algorithm + '_' + dataset + '/result/' + 'experimental_result_' + categories + '-21_bs256_RMSPROP'\
+            + '-' + str(j+1) + '.txt', delimiter='\n')
         f.write(str(data[-1]))
         f.write('\n')
     f.close()

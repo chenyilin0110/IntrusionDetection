ae2ebdf6edef8b97b53e0b8aa8bbda9d0140e652
chenyilin0110 <a0989735018@gmail.com>
2020-07-04 19:36:22 +0800

    add get recall file

--- src/Other/getRecall.py
+++ src/Other/getRecall.py
@@ -6,18 +6,17 @@ dataset = sys.argv[2]
 categories = sys.argv[3]
 
 if dataset == "KDD99":
-    # f = open('src/Plot/' + dataset + '/' + categories + 'class/' + dataset + '_' + categories + 'class_recall_Avg.txt', "a")
-    # data = np.loadtxt('src/' + algorithm + '_' + dataset + '/result/' + 'accuracyprecisionrecall_' + categories + '.txt', delimiter=' ')
-    # f.write(str(data[2]))
-    # f.write(' ')
-    # f.close()
-    print("H")
+    f = open('src/Plot/' + dataset + '/' + categories + 'class/' + dataset + '_' + categories + 'class_recall_Avg.txt', "a")
+    data = np.loadtxt('src/' + algorithm + '_' + dataset + '/result/' + 'accuracyprecisionrecall_' + categories + '.txt', delimiter=' ')
+    f.write(str(data[2]))
+    f.write(' ')
+    f.close()
 else:
-    # f = open('src/Plot/' + dataset + '/' + categories + 'class/' + dataset + '_' + categories + 'class_recall_Avg.txt', "a")
-    # data = np.loadtxt('src/' + algorithm + '_' + dataset + '/result/' + 'accuracyprecisionrecall_' + categories + '.txt', delimiter=' ')
-    # f.write(str(data[2]))
-    # f.write(' ')
-    # f.close()
+    f = open('src/Plot/' + dataset + '/' + categories + 'class/' + dataset + '_' + categories + 'class_recall_Avg.txt', "a")
+    data = np.loadtxt('src/' + algorithm + '_' + dataset + '/result/' + 'accuracyprecisionrecall_' + categories + '.txt', delimiter=' ')
+    f.write(str(data[2]))
+    f.write(' ')
+    f.close()
     # Test-21
     f = open('src/Plot/' + dataset + '/' + categories + 'class/' + dataset + '_' + categories + 'class_recall_Avg-21.txt', "a")
     data = np.loadtxt('src/' + algorithm + '_' + dataset + '/result/' + 'accuracyprecisionrecall_' + categories + '-21.txt', delimiter=' ')

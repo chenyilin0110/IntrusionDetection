aeb5f5e2ed7c528bff579fd7c0b817bb6853c27f
chenyilin0110 <a0989735018@gmail.com>
2020-07-04 19:07:53 +0800

    add precision and recall gnuplot

--- script/Gnuplot.sh
+++ script/Gnuplot.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# accuracy
 # NSL-KDD 2 categories
 gnuplot src/Plot/NSL-KDD/2class/Bar_Graph.plt
 gnuplot src/Plot/NSL-KDD/2class/Bar_Graph-21.plt
@@ -22,3 +23,51 @@ gnuplot src/Plot/KDD99/2class/Bar_Graph.plt
 
 # KDD99 5 categories
 gnuplot src/Plot/KDD99/5class/Bar_Graph.plt
+
+# precision
+# NSL-KDD 2 categories
+gnuplot src/Plot/NSL-KDD/2class/Bar_Graph_precision.plt
+gnuplot src/Plot/NSL-KDD/2class/Bar_Graph_precision-21.plt
+
+# NSL-KDD 5 categories
+gnuplot src/Plot/NSL-KDD/5class/Bar_Graph_precision.plt
+gnuplot src/Plot/NSL-KDD/5class/Bar_Graph_precision-21.plt
+
+# ICS 2 categories
+gnuplot src/Plot/ICS/2class/Bar_Graph_precision.plt
+
+# ICS 3 categories
+gnuplot src/Plot/ICS/3class/Bar_Graph_precision.plt
+
+# ICS multi categories
+gnuplot src/Plot/ICS/multi/Bar_Graph_precision.plt
+
+# KDD99 2 categories
+gnuplot src/Plot/KDD99/2class/Bar_Graph_precision.plt
+
+# KDD99 5 categories
+gnuplot src/Plot/KDD99/5class/Bar_Graph_precision.plt
+
+# recall
+# NSL-KDD 2 categories
+gnuplot src/Plot/NSL-KDD/2class/Bar_Graph_recall.plt
+gnuplot src/Plot/NSL-KDD/2class/Bar_Graph_recall-21.plt
+
+# NSL-KDD 5 categories
+gnuplot src/Plot/NSL-KDD/5class/Bar_Graph_recall.plt
+gnuplot src/Plot/NSL-KDD/5class/Bar_Graph_recall-21.plt
+
+# ICS 2 categories
+gnuplot src/Plot/ICS/2class/Bar_Graph_recall.plt
+
+# ICS 3 categories
+gnuplot src/Plot/ICS/3class/Bar_Graph_recall.plt
+
+# ICS multi categories
+gnuplot src/Plot/ICS/multi/Bar_Graph_recall.plt
+
+# KDD99 2 categories
+gnuplot src/Plot/KDD99/2class/Bar_Graph_recall.plt
+
+# KDD99 5 categories
+gnuplot src/Plot/KDD99/5class/Bar_Graph_recall.plt

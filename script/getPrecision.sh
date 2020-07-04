9f573b90c4b281514913802a7d64baae09e2d100
chenyilin0110 <a0989735018@gmail.com>
2020-07-04 16:31:14 +0800

    add getPrecision and getRecall files

---
+++ script/getPrecision.sh
@@ -0,0 +1,49 @@
+dataset="KDD99"
+categories="2"
+python3 src/Other/getPrecision.py DAE $dataset $categories
+python3 src/Other/getPrecision.py SNDAE $dataset $categories
+python3 src/Other/getPrecision.py DNN $dataset $categories
+python3 src/Other/getPrecision.py DE_DNN $dataset $categories
+python3 src/Other/getPrecision.py LSTM $dataset $categories
+python3 src/Other/getPrecision.py DE_LSTM $dataset $categories
+python3 src/Other/getPrecision.py Naive_Bayes $dataset $categories
+python3 src/Other/getPrecision.py RandomForest $dataset $categories
+python3 src/Other/getPrecision.py SVM $dataset $categories
+python3 src/Other/getPrecision.py KNN $dataset $categories
+
+categories="5"
+python3 src/Other/getPrecision.py DAE $dataset $categories
+python3 src/Other/getPrecision.py SNDAE $dataset $categories
+python3 src/Other/getPrecision.py DNN $dataset $categories
+python3 src/Other/getPrecision.py DE_DNN $dataset $categories
+python3 src/Other/getPrecision.py LSTM $dataset $categories
+python3 src/Other/getPrecision.py DE_LSTM $dataset $categories
+python3 src/Other/getPrecision.py Naive_Bayes $dataset $categories
+python3 src/Other/getPrecision.py RandomForest $dataset $categories
+python3 src/Other/getPrecision.py SVM $dataset $categories
+python3 src/Other/getPrecision.py KNN $dataset $categories
+
+dataset="NSL-KDD"
+categories="2"
+python3 src/Other/getPrecision.py DAE $dataset $categories
+python3 src/Other/getPrecision.py SNDAE $dataset $categories
+python3 src/Other/getPrecision.py DNN $dataset $categories
+python3 src/Other/getPrecision.py DE_DNN $dataset $categories
+python3 src/Other/getPrecision.py LSTM $dataset $categories
+python3 src/Other/getPrecision.py DE_LSTM $dataset $categories
+python3 src/Other/getPrecision.py Naive_Bayes $dataset $categories
+python3 src/Other/getPrecision.py RandomForest $dataset $categories
+python3 src/Other/getPrecision.py SVM $dataset $categories
+python3 src/Other/getPrecision.py KNN $dataset $categories
+
+categories="5"
+python3 src/Other/getPrecision.py DAE $dataset $categories
+python3 src/Other/getPrecision.py SNDAE $dataset $categories
+python3 src/Other/getPrecision.py DNN $dataset $categories
+python3 src/Other/getPrecision.py DE_DNN $dataset $categories
+python3 src/Other/getPrecision.py LSTM $dataset $categories
+python3 src/Other/getPrecision.py DE_LSTM $dataset $categories
+python3 src/Other/getPrecision.py Naive_Bayes $dataset $categories
+python3 src/Other/getPrecision.py RandomForest $dataset $categories
+python3 src/Other/getPrecision.py SVM $dataset $categories
+python3 src/Other/getPrecision.py KNN $dataset $categories
---
+++ script/getRecall.sh
@@ -0,0 +1,49 @@
+dataset="KDD99"
+categories="2"
+python3 src/Other/getRecall.py DAE $dataset $categories
+python3 src/Other/getRecall.py SNDAE $dataset $categories
+python3 src/Other/getRecall.py DNN $dataset $categories
+python3 src/Other/getRecall.py DE_DNN $dataset $categories
+python3 src/Other/getRecall.py LSTM $dataset $categories
+python3 src/Other/getRecall.py DE_LSTM $dataset $categories
+python3 src/Other/getRecall.py Naive_Bayes $dataset $categories
+python3 src/Other/getRecall.py RandomForest $dataset $categories
+python3 src/Other/getRecall.py SVM $dataset $categories
+python3 src/Other/getRecall.py KNN $dataset $categories
+
+categories="5"
+python3 src/Other/getRecall.py DAE $dataset $categories
+python3 src/Other/getRecall.py SNDAE $dataset $categories
+python3 src/Other/getRecall.py DNN $dataset $categories
+python3 src/Other/getRecall.py DE_DNN $dataset $categories
+python3 src/Other/getRecall.py LSTM $dataset $categories
+python3 src/Other/getRecall.py DE_LSTM $dataset $categories
+python3 src/Other/getRecall.py Naive_Bayes $dataset $categories
+python3 src/Other/getRecall.py RandomForest $dataset $categories
+python3 src/Other/getRecall.py SVM $dataset $categories
+python3 src/Other/getRecall.py KNN $dataset $categories
+
+dataset="NSL-KDD"
+categories="2"
+python3 src/Other/getRecall.py DAE $dataset $categories
+python3 src/Other/getRecall.py SNDAE $dataset $categories
+python3 src/Other/getRecall.py DNN $dataset $categories
+python3 src/Other/getRecall.py DE_DNN $dataset $categories
+python3 src/Other/getRecall.py LSTM $dataset $categories
+python3 src/Other/getRecall.py DE_LSTM $dataset $categories
+python3 src/Other/getRecall.py Naive_Bayes $dataset $categories
+python3 src/Other/getRecall.py RandomForest $dataset $categories
+python3 src/Other/getRecall.py SVM $dataset $categories
+python3 src/Other/getRecall.py KNN $dataset $categories
+
+categories="5"
+python3 src/Other/getRecall.py DAE $dataset $categories
+python3 src/Other/getRecall.py SNDAE $dataset $categories
+python3 src/Other/getRecall.py DNN $dataset $categories
+python3 src/Other/getRecall.py DE_DNN $dataset $categories
+python3 src/Other/getRecall.py LSTM $dataset $categories
+python3 src/Other/getRecall.py DE_LSTM $dataset $categories
+python3 src/Other/getRecall.py Naive_Bayes $dataset $categories
+python3 src/Other/getRecall.py RandomForest $dataset $categories
+python3 src/Other/getRecall.py SVM $dataset $categories
+python3 src/Other/getRecall.py KNN $dataset $categories

7a97343b362c1a87105d53a8100420e4a163023f
chenyilin0110 <a0989735018@gmai.com>
2020-07-13 17:59:32 +0800

    add DE_LSTM experimental result

---
+++ src/DE_LSTM_NSL-KDD/result/parameters/parameter.py
@@ -0,0 +1,205 @@
+import numpy as np
+
+f = open('src/Plot/parameter.txt', "a")
+
+result = "DE_LSTM_50101"
+count = 0.0
+for i in range(1,3):
+    data = np.loadtxt('src/DE_LSTM_NSL-KDD/result/parameters/' + result + '-' + str(i) + '.txt', dtype=np.float)
+    count = count + data[-1]
+count = count / 2
+f.write(result + ' ' + str(count) + '\n')
+
+result = "DE_LSTM_50103"
+count = 0.0
+for i in range(1,3):
+    data = np.loadtxt('src/DE_LSTM_NSL-KDD/result/parameters/' + result + '-' + str(i) + '.txt', dtype=np.float)
+    count = count + data[-1]
+count = count / 2
+f.write(result + ' ' + str(count) + '\n')
+
+result = "DE_LSTM_50105"
+count = 0.0
+for i in range(1,3):
+    data = np.loadtxt('src/DE_LSTM_NSL-KDD/result/parameters/' + result + '-' + str(i) + '.txt', dtype=np.float)
+    count = count + data[-1]
+count = count / 2
+f.write(result + ' ' + str(count) + '\n')
+
+result = "DE_LSTM_50107"
+count = 0.0
+for i in range(1,3):
+    data = np.loadtxt('src/DE_LSTM_NSL-KDD/result/parameters/' + result + '-' + str(i) + '.txt', dtype=np.float)
+    count = count + data[-1]
+count = count / 2
+f.write(result + ' ' + str(count) + '\n')
+
+result = "DE_LSTM_50109"
+count = 0.0
+for i in range(1,3):
+    data = np.loadtxt('src/DE_LSTM_NSL-KDD/result/parameters/' + result + '-' + str(i) + '.txt', dtype=np.float)
+    count = count + data[-1]
+count = count / 2
+f.write(result + ' ' + str(count) + '\n')
+
+result = "DE_LSTM_50301"
+count = 0.0
+for i in range(1,3):
+    data = np.loadtxt('src/DE_LSTM_NSL-KDD/result/parameters/' + result + '-' + str(i) + '.txt', dtype=np.float)
+    count = count + data[-1]
+count = count / 2
+f.write(result + ' ' + str(count) + '\n')
+
+result = "DE_LSTM_50303"
+count = 0.0
+for i in range(1,3):
+    data = np.loadtxt('src/DE_LSTM_NSL-KDD/result/parameters/' + result + '-' + str(i) + '.txt', dtype=np.float)
+    count = count + data[-1]
+count = count / 2
+f.write(result + ' ' + str(count) + '\n')
+
+result = "DE_LSTM_50305"
+count = 0.0
+for i in range(1,3):
+    data = np.loadtxt('src/DE_LSTM_NSL-KDD/result/parameters/' + result + '-' + str(i) + '.txt', dtype=np.float)
+    count = count + data[-1]
+count = count / 2
+f.write(result + ' ' + str(count) + '\n')
+
+result = "DE_LSTM_50307"
+count = 0.0
+for i in range(1,3):
+    data = np.loadtxt('src/DE_LSTM_NSL-KDD/result/parameters/' + result + '-' + str(i) + '.txt', dtype=np.float)
+    count = count + data[-1]
+count = count / 2
+f.write(result + ' ' + str(count) + '\n')
+
+result = "DE_LSTM_50309"
+count = 0.0
+for i in range(1,3):
+    data = np.loadtxt('src/DE_LSTM_NSL-KDD/result/parameters/' + result + '-' + str(i) + '.txt', dtype=np.float)
+    count = count + data[-1]
+count = count / 2
+f.write(result + ' ' + str(count) + '\n')
+
+result = "DE_LSTM_50501"
+count = 0.0
+for i in range(1,3):
+    data = np.loadtxt('src/DE_LSTM_NSL-KDD/result/parameters/' + result + '-' + str(i) + '.txt', dtype=np.float)
+    count = count + data[-1]
+count = count / 2
+f.write(result + ' ' + str(count) + '\n')
+
+result = "DE_LSTM_50503"
+count = 0.0
+for i in range(1,3):
+    data = np.loadtxt('src/DE_LSTM_NSL-KDD/result/parameters/' + result + '-' + str(i) + '.txt', dtype=np.float)
+    count = count + data[-1]
+count = count / 2
+f.write(result + ' ' + str(count) + '\n')
+
+result = "DE_LSTM_50505"
+count = 0.0
+for i in range(1,3):
+    data = np.loadtxt('src/DE_LSTM_NSL-KDD/result/parameters/' + result + '-' + str(i) + '.txt', dtype=np.float)
+    count = count + data[-1]
+count = count / 2
+f.write(result + ' ' + str(count) + '\n')
+
+result = "DE_LSTM_50507"
+count = 0.0
+for i in range(1,3):
+    data = np.loadtxt('src/DE_LSTM_NSL-KDD/result/parameters/' + result + '-' + str(i) + '.txt', dtype=np.float)
+    count = count + data[-1]
+count = count / 2
+f.write(result + ' ' + str(count) + '\n')
+
+result = "DE_LSTM_50509"
+count = 0.0
+for i in range(1,3):
+    data = np.loadtxt('src/DE_LSTM_NSL-KDD/result/parameters/' + result + '-' + str(i) + '.txt', dtype=np.float)
+    count = count + data[-1]
+count = count / 2
+f.write(result + ' ' + str(count) + '\n')
+
+result = "DE_LSTM_50701"
+count = 0.0
+for i in range(1,3):
+    data = np.loadtxt('src/DE_LSTM_NSL-KDD/result/parameters/' + result + '-' + str(i) + '.txt', dtype=np.float)
+    count = count + data[-1]
+count = count / 2
+f.write(result + ' ' + str(count) + '\n')
+
+result = "DE_LSTM_50703"
+count = 0.0
+for i in range(1,3):
+    data = np.loadtxt('src/DE_LSTM_NSL-KDD/result/parameters/' + result + '-' + str(i) + '.txt', dtype=np.float)
+    count = count + data[-1]
+count = count / 2
+f.write(result + ' ' + str(count) + '\n')
+
+result = "DE_LSTM_50705"
+count = 0.0
+for i in range(1,3):
+    data = np.loadtxt('src/DE_LSTM_NSL-KDD/result/parameters/' + result + '-' + str(i) + '.txt', dtype=np.float)
+    count = count + data[-1]
+count = count / 2
+f.write(result + ' ' + str(count) + '\n')
+
+result = "DE_LSTM_50707"
+count = 0.0
+for i in range(1,3):
+    data = np.loadtxt('src/DE_LSTM_NSL-KDD/result/parameters/' + result + '-' + str(i) + '.txt', dtype=np.float)
+    count = count + data[-1]
+count = count / 2
+f.write(result + ' ' + str(count) + '\n')
+
+result = "DE_LSTM_50709"
+count = 0.0
+for i in range(1,3):
+    data = np.loadtxt('src/DE_LSTM_NSL-KDD/result/parameters/' + result + '-' + str(i) + '.txt', dtype=np.float)
+    count = count + data[-1]
+count = count / 2
+f.write(result + ' ' + str(count) + '\n')
+
+result = "DE_LSTM_50901"
+count = 0.0
+for i in range(1,3):
+    data = np.loadtxt('src/DE_LSTM_NSL-KDD/result/parameters/' + result + '-' + str(i) + '.txt', dtype=np.float)
+    count = count + data[-1]
+count = count / 2
+f.write(result + ' ' + str(count) + '\n')
+
+result = "DE_LSTM_50903"
+count = 0.0
+for i in range(1,3):
+    data = np.loadtxt('src/DE_LSTM_NSL-KDD/result/parameters/' + result + '-' + str(i) + '.txt', dtype=np.float)
+    count = count + data[-1]
+count = count / 2
+f.write(result + ' ' + str(count) + '\n')
+
+result = "DE_LSTM_50905"
+count = 0.0
+for i in range(1,3):
+    data = np.loadtxt('src/DE_LSTM_NSL-KDD/result/parameters/' + result + '-' + str(i) + '.txt', dtype=np.float)
+    count = count + data[-1]
+count = count / 2
+f.write(result + ' ' + str(count) + '\n')
+
+result = "DE_LSTM_50907"
+count = 0.0
+for i in range(1,3):
+    data = np.loadtxt('src/DE_LSTM_NSL-KDD/result/parameters/' + result + '-' + str(i) + '.txt', dtype=np.float)
+    count = count + data[-1]
+count = count / 2
+f.write(result + ' ' + str(count) + '\n')
+
+result = "DE_LSTM_50909"
+count = 0.0
+for i in range(1,3):
+    data = np.loadtxt('src/DE_LSTM_NSL-KDD/result/parameters/' + result + '-' + str(i) + '.txt', dtype=np.float)
+    count = count + data[-1]
+count = count / 2
+f.write(result + ' ' + str(count) + '\n')
+
+f.close()
